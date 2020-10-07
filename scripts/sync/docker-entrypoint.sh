e6a277136515a9630788a40c78e8cb0033bfb4f8
valentinab25 <valentinab25>
2020-10-07 12:22:38 +0300

    added emails

--- scripts/sync/docker-entrypoint.sh
+++ scripts/sync/docker-entrypoint.sh
@@ -6,18 +6,37 @@ generalsummary=/logs/history.log
 lastfilerun=/logs/lastfilescopied.log
 lastsuccrun=/logs/lastsuccessrun.log
 
+
+POSTFIX_SERVER=${POSTFIX_SERVER:-"postfix"}
+FROM_ADDRESS=${FROM_ADDRESS:-"noreply@eea.europa.eu"}
+
 set -o pipefail
 
+flag=-v
 if [ -n "$DEBUG" ]; then
    flag=-vv
 fi
 
+#configure email
+
+sed -i "s/.*mailhub=.*/mailhub=$POSTFIX_SERVER//" /etc/ssmtp/ssmtp.conf
+sed -i "s/.*rewriteDomain=.*/rewriteDomain=eea.europa.eu//" /etc/ssmtp/ssmtp.conf
+if [ $(grep ^FromLineOverride /etc/ssmtp/ssmtp.conf | wc -l ) -eq 0 ]; then
+      echo "FromLineOverride=YES" >> /etc/ssmtp/ssmtp.conf
+fi
 
 
 rclone sync $flag remote:$SOURCE_DIRECTORY $TARGET_DIRECTORY 2>&1 | tee -a $logname
 
 if [ $? -ne 0 ]; then
-        echo "Sync command executed with ERROR"
+        echo "Sync command executed with ERROR, sending email"
+	sendmail -f ${FROM_ADDRESS} ${TO_ADDRESS}  <<EOF
+From: ${FROM_ADDRESS}
+Subject: ERROR copernicus landfiles sync on $(hostname)
+
+$(cat $logname)
+EOF
+        exit 1
 else
         echo "Command finished with success" >> $logname
         echo "Last sync with transferred files on - $current_date" > $lastsuccrun
@@ -40,6 +59,20 @@ else
 	echo "Sync finished without transferring any new files"
 fi
 
+if [ -n "$ALLWAYS_SEND_EMAIL" ]; then
+               sendmail -f ${FROM_ADDRESS} ${TO_ADDRESS}  <<EOF
+From: ${FROM_ADDRESS}
+Subject: INFO copernicus landfiles sync summary on $(hostname)
+
+Last run summary:
+$(cat $lastsuccrun)
+=========================================
+$(cat $lastfilerun)
+=========================================
+EOF
+fi
+
+
 duration=$(grep -E '^Elapsed time:' $logname | awk '{print $3}')
 
 HOURS_MAX_DURATION=${HOURS_MAX_DURATION:-6}
@@ -59,6 +92,13 @@ done
 
 if [ $sum -gt $HOURS_MAX_DURATION ];then
 	echo "Sync duration is more than $HOURS_MAX_DURATION hours, will send email"
+        sendmail -f ${FROM_ADDRESS} ${TO_ADDRESS}  <<EOF
+From: ${FROM_ADDRESS}
+Subject: WARNING copernicus-land landfiles sync takes too long on $(hostname)
+
+$(cat $logname)
+EOF
+
 fi
 
 
@@ -66,7 +106,14 @@ HOURS_WITHOUT_RUN=${HOURS_WITHOUT_RUN:-24}
 let mins=60*$HOURS_WITHOUT_RUN
 
 if [ $(grep "Command finished with success" $(find /logs -type f -mmin -$mins) | wc -l) -eq 0 ]; then
-  echo "No succesfull runs in the last $HOURS_WITHOUT_RUN hours, will send email"
+          echo "No succesfull runs in the last $HOURS_WITHOUT_RUN hours, will send email"
+          sendmail -f ${FROM_ADDRESS} ${TO_ADDRESS}  <<EOF
+From: ${FROM_ADDRESS}
+Subject: ERROR No copernicus landfiles syncs in $HOURS_WITHOUT_RUN h on $(hostname)
+
+$(cat $logname)
+EOF
+
 fi
 
 
